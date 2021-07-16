24454a8d3fce58cfbabd885747d0e2d73bec0a03
seoha <devsh4030@gmail.com>
2021-07-16 15:16:52 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+let MAX_AGE = 100;
+
+interface IPerson{
+    name : string
+    age : number
+}
+
+class Person implements IPerson{
+    constructor(public name:string, public age : number){}
+}
+
+function makeRandomNumber(max:number = MAX_AGE):number {
+    return Math.ceil((Math.random()*max))
+}
+
+const makePerson = (name:String, age:number = makeRandomNumber() )=>({name, age})
+
+const testMakePerson = ():void =>{
+    let jane:IPerson = makePerson('jane')
+    let jack:IPerson = makePerson('jack')
+    console.log(jane, jack)
+}
+
+testMakePerson()
